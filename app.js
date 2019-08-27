c332a24f407c9d6360281b2bfb745833c695ea55
jbj616 <jbj616@gmail.com>
2019-08-27 13:48:19 +0900

    node init

---
+++ app.js
@@ -0,0 +1,36 @@
+//module
+const express = require("express");
+const http = require("http");
+const expressErrorHandler = require("express-error-handler");
+const fs = require("fs");
+
+// //변수
+const app = express();
+const router = express.Router();
+
+//설정
+app.set("port", process.env.PORT || 3000);
+
+app.get('/', (req, res)=>{
+  console.log('get(/) 실행됨.');
+  
+  fs.readFile('./public/index.html', (err, data)=>{
+    res.writeHead(200, {'Content-Type':'text/html;charset=utf-8'});
+    res.end(data);
+  });
+});
+
+//에러 처리
+var errorHandler = expressErrorHandler({
+  static: {
+    "404": "public/index.html"
+  }
+});
+
+app.use(expressErrorHandler.httpError(404));
+app.use(errorHandler);
+
+//서버 생성
+http.createServer(app).listen(app.get("port"), () => {
+  console.log("익스프레스로 웹 서버를 실행함: " + app.get("port"));
+});
