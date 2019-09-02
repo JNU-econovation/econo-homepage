1687a804ffcb78eee656ce47b4b9a0d535fc9897
jbj616 <jbj616@gmail.com>
2019-09-02 20:15:45 +0900

    fix : 지원하기

--- front/assets/js/invitation.js
+++ front/assets/js/invitation.js
@@ -16,32 +16,32 @@ function showSlides(n) {
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {
     slideIndex = 1
-  } 
+  }
   if (n < 1) {
     slideIndex = slides.length
   }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  if(slideIndex>0){
-  slides[slideIndex-1].style.display = "block"; 
+  if (slideIndex > 0) {
+    slides[slideIndex - 1].style.display = "block";
   }
-  
+
 }
 
 function showImages() {
-  var str='';
-  for(var i=1; i<=11; i++){
-    str+=`<div class="mySlides fade">
+  var str = '';
+  for (var i = 1; i <= 11; i++) {
+    str += `<div class="mySlides fade" >
     <div class="numbertext">${i} / 11</div>
     <img src="/images/card${i}.jpg" style="width:100%">
 </div>`
   }
 
-  str+=`<!-- buttons -->
+  str += `<!-- buttons -->
   <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
   <a class="next" onclick="plusSlides(1)">&#10095;</a>`
 
