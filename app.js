a03ab884511979a979b15cdce4cf8e3913502056
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2022-01-17 15:36:21 +0900

    feat: 2022_winter_dev dev페이지 추가

--- app.js
+++ app.js
@@ -23,74 +23,78 @@ app.set("views", __dirname + "/views");
 app.use("/", static(path.join(__dirname, "views/page")));
 
 app.get("/", (req, res) => {
-  res.redirect("/about");
+    res.redirect("/about");
 });
 
 app.get("/recruit-2021-1", (req, res) => {
-  res.render("page/recruit/recruit_2021");
+    res.render("page/recruit/recruit_2021");
 });
 
 app.get("/about_econo", (req, res) => {
-  res.render("page/about");
+    res.render("page/about");
 });
 
 app.get("/about", (req, res) => {
-  res.render("page/about");
+    res.render("page/about");
 });
 
 app.get("/hackathon-2019", (req, res) => {
-  res.render("page/hackathon/hackathon_2019");
+    res.render("page/hackathon/hackathon_2019");
 });
 
 app.get("/hackathon-2020", (req, res) => {
-  res.render("page/hackathon/hackathon_2020", { test: process.env.AKECONO });
+    res.render("page/hackathon/hackathon_2020", { test: process.env.AKECONO });
 });
 
 app.get("/hackathon-2021", (req, res) => {
-  res.render("page/hackathon/hackathon_2021", { test: process.env.AKECONO });
+    res.render("page/hackathon/hackathon_2021", { test: process.env.AKECONO });
 });
 
 app.get("/summer-dev-2020", (req, res) => {
-  res.render("page/dev/summer_dev_2020");
+    res.render("page/dev/summer_dev_2020");
 });
 
 app.get("/winter-dev-2021", (req, res) => {
-  res.render("page/dev/winter_dev_2021");
+    res.render("page/dev/winter_dev_2021");
 });
 
 app.get("/summer-dev-2021", (req, res) => {
-  res.render("page/dev/summer_dev_2021");
+    res.render("page/dev/summer_dev_2021");
+});
+
+app.get("/winter-dev-2022", (req, res) => {
+    res.render("page/dev/winter_dev_2022");
 });
 
 app.get("/portfolio-2021-summer", (req, res) => {
-  res.render("page/portfolio/portfolio_2021_summer", {
-    data: summer_2021_data,
-  });
+    res.render("page/portfolio/portfolio_2021_summer", {
+        data: summer_2021_data,
+    });
 });
 
 app.get("/portfolio-2021-winter", (req, res) => {
-  res.render("page/portfolio/portfolio_2021_winter", {
-    data: winter_2021_data,
-  });
+    res.render("page/portfolio/portfolio_2021_winter", {
+        data: winter_2021_data,
+    });
 });
 
 app.get("/portfolio-2020-summer", (req, res) => {
-  res.render("page/portfolio/portfolio_2020_summer");
+    res.render("page/portfolio/portfolio_2020_summer");
 });
 
 app.get("/portfolio-2020-winter", (req, res) => {
-  res.render("page/portfolio/portfolio_2020_winter");
+    res.render("page/portfolio/portfolio_2020_winter");
 });
 
 app.get("/faq", (req, res) => {
-  res.render("page/faq");
+    res.render("page/faq");
 });
 
 //에러 처리
 var errorHandler = expressErrorHandler({
-  static: {
-    404: "./front/erroor.html",
-  },
+    static: {
+        404: "./front/erroor.html",
+    },
 });
 
 app.use(expressErrorHandler.httpError(404));
@@ -98,5 +102,5 @@ app.use(errorHandler);
 
 //서버 생성
 http.createServer(app).listen(app.get("port"), () => {
-  console.log("익스프레스로 웹 서버를 실행함: " + app.get("port"));
+    console.log("익스프레스로 웹 서버를 실행함: " + app.get("port"));
 });
