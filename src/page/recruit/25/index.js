8fd40c155dc22ff43334343c04314ca33ffa0958
loopy <loopy@jnu.ac.kr>
2023-03-03 04:48:07 +0000

    feat(recruit): add 25 recruite page
    Signed-off-by: loopy <loopy@jnu.ac.kr>

---
+++ src/page/recruit/25/index.js
@@ -0,0 +1,20 @@
+import Navigation from "../../../components/Navigation";
+import Recruit25Component from "../../../components/recruit/25/index_2";
+import Footer from "../../../components/Footer";
+
+function Recruit25() {
+  // let doc = document.querySelector("#titleBar .toggle");
+  // doc.classList.add("pur");
+  // let but = document.querySelectorAll(".button.primary");
+  // but.forEach((el) => el.classList.add("pur"));
+
+  return (
+    <div id="page-wrapper">
+      <Navigation />
+      <Recruit25Component />
+      <Footer />
+    </div>
+  );
+}
+
+export default Recruit25;
