307815ebc2eb73e00b24a6ebccefd8ff5fec9c73
loopy-lim <loopy@jnu.ac.kr>
2023-07-24 21:14:47 +0900

    refactor: fix faq

--- src/components/recruit/Faq.component.tsx
+++ src/components/recruit/Faq.component.tsx
@@ -1,43 +1,23 @@
 import { FAQ } from "@/src/assets/constants/faq.ko";
-import { gsap } from "gsap";
+import gsap from "gsap";
+import Image from "next/image";
 import { useState } from "react";
 
 const Faq = () => {
   const [faqBoard, setFaqBoard] = useState(0);
   const [faqOpenPosition, setFaqOpenPosition] = useState(-1);
 
-  const faqOpen = (position: number) => {
-    gsap.context(() => {
-      gsap.to(`.faq-content-${position}`, {
-        color: "#fff",
-        zIndex: 10,
-      });
-      gsap.to(`.faq-backgorund-${position}`, {
-        width: "190%",
-        zIndex: 5,
-      });
-      gsap.to(`.faq-subcontent-${position}`, {
-        autoAlpha: 1,
-        zIndex: 10,
-      });
+  const faqOpen = (index: number) => {
+    gsap.to(`#faq-${index}`, {
+      height: "auto",
+      duration: 0.5,
     });
   };
 
-  const faqClose = (position: number) => {
-    gsap.context(() => {
-      gsap.to(`.faq-content-${position}`, {
-        color: "#000",
-        zIndex: 0,
-      });
-      gsap.to(`.faq-backgorund-${position}`, {
-        width: "0%",
-        zIndex: 0,
-      });
-      gsap.to(`.faq-subcontent-${position}`, {
-        color: "#fff",
-        autoAlpha: 0,
-        zIndex: -10,
-      });
+  const faqClose = (index: number) => {
+    gsap.to(`#faq-${index}`, {
+      height: "0",
+      duration: 0.5,
     });
   };
 
@@ -53,12 +33,13 @@ const Faq = () => {
     }
     setFaqOpenPosition(position);
   };
-
   return (
     <>
       <div className="flex justify-between my-8">
         <h1>FAQ</h1>
-        <div className="flex gap-10 text-2xl">
+      </div>
+      <div className="flex gap-8">
+        <div className="flex flex-col gap-12 text-xl flex-1 items-start">
           {FAQ.map((d, i) => (
             <button
               className={faqBoard === i ? "" : "text-[#AFAFAF]"}
@@ -72,51 +53,31 @@ const Faq = () => {
             </button>
           ))}
         </div>
-      </div>
-      <div className="grid grid-cols-2">
-        {FAQ[faqBoard].DATA.map((d, i) => (
-          <div
-            className="py-14 text-2xl border-b border-black odd:border-r first:border-t [&:nth-child(2)]:border-t relative"
-            key={i}
-          >
-            <input
-              type="radio"
-              name="faq"
-              id={`faq-${i}`}
-              value={faqOpenPosition}
-              className="hidden"
-              onClick={() => faqToggle(i)}
-            />
-            <label htmlFor={`faq-${i}`}>
-              <div
-                className={`faq-content-${i} absolute top-2 left-4 text-2xl`}
+        <div className="grid grid-cols-2 gap-12 flex-[2_0_0%] -translate-y-8">
+          {FAQ[faqBoard].DATA.map((data, index) => (
+            <div className="w-full border-b" key={index}>
+              <button
+                className="flex items-center py-8 w-full relative group [&>*]:transition-all [&>*]:duration-500 [&>*]:ease-in-out"
+                onClick={() => faqToggle(index)}
               >
-                {`${i + 1}`.padStart(2, "0")}
+                <div className="text-base font-semibold group-hover:translate-x-4">
+                  {`${index + 1}`.padStart(2, "0")}
+                </div>
+                <div className="ml-6 text-lg group-hover:translate-x-4 delay-100">
+                  {data.Q}
+                </div>
+                <Image
+                  className="w-4 h-4 absolute right-0 group-hover:-translate-x-4"
+                  src={require("/public/icons/multiply.svg").default}
+                  alt="see more"
+                />
+              </button>
+              <div className="ml-3 overflow-hidden h-0" id={`faq-${index}`}>
+                <div className="p-8">{data.A}</div>
               </div>
-              <div
-                className={`faq-content-${i} absolute top-[50%] -translate-y-4 left-0 text-center w-[100%] h-[100%]`}
-              >
-                {d.Q}
-              </div>
-              <div
-                className={
-                  `faq-backgorund-${i} absolute top-0 bg-black h-[calc(3px+100%)] translate-y-[-1px] ` +
-                  (i % 2 === 0 ? "left-0" : "right-0")
-                }
-              ></div>
-              <div
-                className={
-                  `faq-content-${i} faq-subcontent-${i} absolute text-transparent top-[50%] w-[90%] translate-y-[-50%] text-center text-lg -z-10 text-white ` +
-                  (i % 2 === 0 ? "left-[90%]" : "right-[90%]")
-                }
-              >
-                {d.A.split("\n").map((ad, ai) => (
-                  <div key={ai}>{ad}</div>
-                ))}
-              </div>
-            </label>
-          </div>
-        ))}
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
