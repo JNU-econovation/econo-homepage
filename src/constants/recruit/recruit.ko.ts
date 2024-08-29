32135a08f9af24faff5caddedfef1eccae681033
2yunseong <dbsdltjd123@gmail.com>
2024-08-30 00:22:55 +0900

    fix: 시작/마감 시간 최신화
    9/2 09:00 ~ 9/15 223:59:59

--- src/constants/recruit/recruit.ko.ts
+++ src/constants/recruit/recruit.ko.ts
@@ -8,7 +8,7 @@ const RECRUIT = {
   CONTENT:
     "에코노베이션에서 함께할 여러분을 모집합니다.\n에코노베이션은 지식의 선순환이 자연스럽게 이루어지는 환경을 만드는 것을 목표하고 있습니다.\n개발에 열정이 있다면 에코노베이션에 들어와 지식의 선순환을 일으켜주세요.",
   IS_ON: true /** FIXME: This property was deprecated. */,
-  START_DATE: Date.UTC(2024, 7, 2, 9, 0, 0),
+  START_DATE: Date.UTC(2024, 8, 2, 9, 0, 0),
   END_DATE: Date.UTC(2024, 8, 15, 23, 59, 59),
   GENERTAION: 28,
   SCHEDULE: [
