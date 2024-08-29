8edc3a9c76293bc62f3806e4342d660dac8ad81c
2yunseong <dbsdltjd123@gmail.com>
2024-08-30 01:38:49 +0900

    refactor: 조건부 렌더링을 객체의 속성을 사용하는 것으로 변경

--- src/components/main/RecruitFloat/HoverText.tsx
+++ src/components/main/RecruitFloat/HoverText.tsx
@@ -9,15 +9,16 @@ interface HoverTextProps {
 }
 
 const HoverText = ({ status, days }: HoverTextProps) => {
-  if (status === "READY")
-    return (
+  const hoverComponents = {
+    READY: (
       <span>
         {RECRUIT_FLOAT.ECONO_READY_FOR_RECRUIT} D-{days}
       </span>
-    );
-  else if (status === "OPEN")
-    return <span>{RECRUIT_FLOAT.ECONO_IS_RECRUITING}</span>;
-  else return <span></span>;
+    ),
+    OPEN: <span>{RECRUIT_FLOAT.ECONO_IS_RECRUITING}</span>,
+    CLOSED: <span />,
+  };
+  return hoverComponents[status] ?? <span />;
 };
 
 export default HoverText;
